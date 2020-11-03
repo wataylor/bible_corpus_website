98e68cacaf0e1b5152199fc16c600e5c1971cd20
Bill Taylor <wataylor@alum.mit.edu>
2020-11-02 21:08:09 -0500

    update DB, write error files per book, 4 hours for 108 Bibles

--- src/test/java/asst/loaders/LoadCorpusBibleFilesMain.java
+++ src/test/java/asst/loaders/LoadCorpusBibleFilesMain.java
@@ -16,6 +16,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import asst.common.CommandArgs;
+import asst.corpus.utils.BookNameAbbrevMap;
 import asst.dbase.Quotable;
 import asst.dbcommon.PUTs;
 import utils.ConvUtils;
@@ -31,8 +32,11 @@ import utils.Vbls;
 public class LoadCorpusBibleFilesMain {
 	static final String[] DEFAULT_ARGS = {
 			"path=F:\\git\\bible-corpus\\bibles",
-			"outdir=F:\\MySQLData", "-verbose", "-doIt",
+			"outdir=F:\\MySQLData", "-verbose", "+doIt",
 			"limit=3", "lang=placeholder",
+			"driver=com.mysql.jdbc.Driver",
+			"url=jdbc:mysql://localhost/biblecorpus?user=root&useUnicode=yes&characterEncoding=UTF-8",
+			"user=NONE", "pass=NONE"
 	};
 	/* Global variables are a sleazy way to avoid passing
 	 * lots of method arguments.*/
@@ -41,6 +45,9 @@ public class LoadCorpusBibleFilesMain {
 	static String priorTable = "";
 	static int currentTable;
 	static PrintStream updates = null;
+	static PrintStream insertStats = null;
+	static PrintStream objections = null;
+	static String msg = null;
 
 	/** Read a list of files and generate insert statements
 	 * for each file found.
@@ -50,26 +57,42 @@ public class LoadCorpusBibleFilesMain {
 		CommandArgs cargs = new CommandArgs(DEFAULT_ARGS);
 		Consumer<? super Path> csp = a -> handleAFile(a);
 		vbls = new Vbls(cargs, args);
+		if (vbls.doIt) {
+			vbls.openDB();
+		}
 		File inputDir = new File(vbls.path);
 		if (!inputDir.isDirectory()) {
 			System.out.println("path= must specify a directory of corpus input files and outdir= must specify where output files are to be written.");
 			System.exit(-1);
 		}
 		try {
+			insertStats = new PrintStream(new File(vbls.outdir + File.separator + "insertStats.txt"), "UTF8");
 			Files.newDirectoryStream(Paths.get(vbls.path), path -> path.toString().endsWith(".xml")).forEach(csp);
-			System.out.println(vbls.bip.howLongSince(false, true));
+			msg = "Total time " + vbls.bip.howLongSince(false, true);
+			insertStats.println(msg);
+			System.out.println(msg);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			updates.close();
+			insertStats.close();
+			if (vbls.doIt) {
+				vbls.closeDB();
+			}
 		}
 	}
 
 	private static void handleAFile(Path file) {
-		String tbl;
+		String tbl = null;
 		String tblNo = null;
 		String where;
-		String upd;
+		String upd = null;
+		String orig;
+		String verse;
+		int offset;
+		Integer bookNo;
+		java.sql.ResultSet res;
+		int insCount = 0;
 		StringBuilder sb = new StringBuilder(file.getFileName().toString());
 		sb = ConvUtils.replaceAll(sb, ".xml", "");
 		// String pureName = sb.toString();
@@ -93,6 +116,7 @@ public class LoadCorpusBibleFilesMain {
 					updates.close();
 				}
 				updates = new PrintStream(new File(vbls.outdir + File.separator + "updates" + tblNo + ".sql"), "UTF8");
+				priorTable = tblNo;
 			}
 
 			/* Walk all the corpus seg list for each verse*/
@@ -127,31 +151,87 @@ public class LoadCorpusBibleFilesMain {
 						if (!PUTs.isStringMTP(type) && !PUTs.isStringMTP(id)) {
 							String [] s = id.split("\\.");
 							if (s.length == 4) {
-								where = "Code=\"" + s[1] + "\" and Chapter = " + s[2] + " and Verse = " + s[3];
+								offset = 1;
 							} else if (s.length == 3) {
-								where = "Code=\"" + s[0] + "\" and Chapter = " + s[1] + " and Verse = " + s[2];
+								offset = 0;
 							} else {
-								System.out.println("ERR " + colName + " bad id >" + id + "<");
+								msg = "ERR " + colName + " bad id >" + id + "<";
+								insertStats.println(msg);
+								System.out.println(msg);
+								object(colName, msg);
 								continue;
 							}
+							bookNo = BookNameAbbrevMap.bookCodes.get(s[offset]);
+							if (bookNo == null) {
+								msg = "ERR " + colName + " bad book code >" + s[offset] + "< " + id;
+								insertStats.println(msg);
+								object(colName, msg);
+								continue;
+							}
+							where = "Book = " + bookNo + " and Chapter = " + s[offset + 1] + " and Verse = " + s[offset + 2];
+							verse = nNode.getTextContent().trim();
+							res = vbls.stmt.executeQuery("select " + colName + " from " + tbl + " where " + where);
+							if (!res.next()) {
+								msg = "Nothing " + colName + " from " + tbl + " where " + where + " " + id;
+								object(colName, msg);
+							} else {
+								orig = res.getString(1);
+								if (orig == null) {
+/*
+									msg = "ERR Not Written " + colName + " from " + tbl + " where " + where;
+									insertStats.println(msg);
+*/
+								} else if (!verse.contentEquals(orig)) {
+									msg = "Not Same " + colName + " from " + tbl + " where " + where + " " + id;
+									object(colName, msg);
+								}
+							}
+							res.close();
 							upd = "update " + tbl + " set " + colName
-									+ "=" + Quotable.constructSQLCharacterValue(nNode.getTextContent().trim())
-									+ " where " + where + ";";
-							updates.println(upd);
+									+ "=" + Quotable.constructSQLCharacterValue(verse)
+									+ " where " + where;
+							updates.println(upd + ";");
+							if (vbls.doIt) {
+								PUTs.anyStatement(upd, vbls.stmt);
+							}
+							insCount++;
 						} else {
 							System.out.println("ERR " + colName + " missing id or type");
 							continue;
 						}
 					}
 				} else {
-					System.out.println("ERR " + colName + " not element");
+					msg = "ERR " + colName + " not element";
+					System.out.println(msg);
+					insertStats.println(msg);
+					object(colName, msg);
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(-1);
+			insertStats.println("ERR " + upd + " " + e.getMessage());
 		}
-		System.out.println("Tblno " + tblNo + " col " + colName + " " + vbls.ip.howLongSince(false, true));
+		msg = "select count(*) from " + tbl + " where "
+				+ colName + " is not null;"
+				+ " inserts " + insCount + " "
+				+ vbls.ip.howLongSince(false, true);
+		insertStats.println(msg);
+		System.out.println(msg);
 		nameCount++;
+		if (objections != null) {
+			objections.close();
+			objections = null;
+		}
+	}
+
+	/** Generate a file-specific error message
+	 * @param table column name
+	 * @param whine text
+	 * @throws Exception if the stream doesn't open
+	 */
+	public static void object(String table, String whine) throws Exception {
+		if (objections == null) {
+			objections = new PrintStream(new File(vbls.outdir + File.separator + table + ".txt"), "UTF8");
+		}
+		objections.println(whine);
 	}
 }
