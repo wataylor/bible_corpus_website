92a332b73380c7d3dc74d53594f3f4a33feef730
Bill Taylor <wataylor@alum.mit.edu>
2020-11-04 16:27:27 -0500

    Added formal verse reference to table heading and at end of text paragraph

--- src/main/java/asst/corpus/utils/FormatResponseText.java
+++ src/main/java/asst/corpus/utils/FormatResponseText.java
@@ -26,12 +26,22 @@ public class FormatResponseText {
 		boolean multi = (bcv.languageCount > 1);
 		if (multi) {
 			sb.append("<table border=\"1\" cellpadding=\"2\" cellspacing=\"2\">\n");
+			sb.append("<th style=\"text-align:center;\" colspan=\""
+					+ (bcv.languageCount) + "\">"
+					+ bcv.formalReference + "</th></tr>");
+		} else {
+			sb.append("<p>");
 		}
 		for (String el : rows) {
 			formatOneRow(sb, el);
 		}
 		if (multi) {
 			sb.append("</table>\n");
+		} else {
+			StringBuilder sb2 = new StringBuilder(bcv.formalReference);
+			replaceAll(sb2, " ", "&nbsp;");
+			sb.append("&nbsp; <b>" + sb2.toString() + "</b>");
+			sb.append("</p>");
 		}
 	}
 
@@ -74,5 +84,24 @@ public class FormatResponseText {
 		if ("NULL".equalsIgnoreCase(trim)) { return NOTFOUND; }
 		return trim;
 	}
+	  /**
+	   * Replace all occurrences of s1 in the string with s2
+	   * @param sb string to be modified
+	   * @param s1 string to be replaced
+	   * @param s2 replacement string.  If null, changed to the empty string
+	   * @return modified string
+	   */
+	  public static StringBuilder replaceAll(StringBuilder sb,
+						 String s1, String s2) {
+	    int ix  = 0;
+	    int len = s1.length();
+	    if (s2 == null) { s2 = ""; }
+
+	    while ( (ix = sb.indexOf(s1, ix)) >-1) {
+	      sb = sb.replace(ix, ix+len, s2);
+	      ix += s2.length(); // do not search replacement string
+	    }
+	    return sb;
+	  }
 
 }
