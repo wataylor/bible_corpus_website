2d8312851588881255ecf72d67cb273a31544a60
Bill Taylor <wataylor@alum.mit.edu>
2020-11-25 10:56:48 -0500

    look up table definition from lang, deduce path from properties file

--- src/main/java/asst/corpus/ProduceHtml.java
+++ src/main/java/asst/corpus/ProduceHtml.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.ResourceBundle;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import asst.MapLanguagesToTables;
 import asst.corpus.utils.GetBookHTML;
 import asst.dbase.DBProp;
 import asst.dbase.DataBase;
@@ -25,6 +27,9 @@ import asst.dbcommon.PUTs;
 @WebServlet(description = "Generate a .html file containing one translation", urlPatterns = { "/ProduceHtml" })
 public class ProduceHtml extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	/** Tells where to write generated .html. It defaults to the
+	 * directory on the perennity server.   */
+	private static String htmlPath = "/home/perennity/generatedHtml";
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -42,13 +47,26 @@ public class ProduceHtml extends HttpServlet {
 		context = configuration.getServletContext();
 		String fileRoot = context.getRealPath("/");
 
-		System.out.println("\n" + getClass().getSimpleName() + " Init called. Default path " + file.getAbsolutePath() + " context root " + fileRoot);
+		System.out.println(getClass().getSimpleName() + " Init.  Default path "
+				+ file.getAbsolutePath() + " context root " + fileRoot);
 
 		en = configuration.getInitParameterNames();
 		while (en.hasMoreElements()) {
 			nam = en.nextElement();
 			System.out.println(nam + " " + configuration.getInitParameter(nam));
 		}
+	    try {
+			ResourceBundle bundle = ResourceBundle.getBundle("Perennity");
+			String fileOrg = bundle.getString("file");
+			System.out.println("Bundle from " + fileOrg);
+			htmlPath = bundle.getString("path");
+			System.out.println("Files will be written to " + htmlPath);
+	    } catch (Exception e) {
+	    	String msg = "Error with resource file Perennity.properties "
+	    			+ e.toString();
+	    	System.out.println(msg);
+	    }
+	    System.out.println("Writing to " + htmlPath);
 	}
 
 	/**
@@ -64,14 +82,18 @@ public class ProduceHtml extends HttpServlet {
 			throw new ServletException(e.getMessage());
 		}
 		String lang = request.getParameter("lang");
-		String tbl = request.getParameter("tbl");
-		if (PUTs.isStringMTP(lang) || PUTs.isStringMTP(tbl)) {
-			response.getWriter().append("Request must have both a lang and a tbl parameter");
+		if (PUTs.isStringMTP(lang)) {
+			response.getWriter().append("Request must have a lang= parameter to select a translation.");
+			return;
+		}
+		String tbl = MapLanguagesToTables.LANG_2_TABLE.get(lang);
+		if (PUTs.isStringMTP(tbl)) {
+			response.getWriter().append("Unknown language parameter " + lang + ".");
 			return;
 		}
 		System.out.println("lang " + lang + " tbl " + tbl);
 		try {
-			InputStream in = GetBookHTML.getBibleHtml(lang, tbl, "f:/MySQLData", DataBase.connDB);
+			InputStream in = GetBookHTML.getBibleHtml(lang, tbl, htmlPath, DataBase.connDB);
 			ServletOutputStream  out = response.getOutputStream();
 			int length;
 			byte[] bytes = new byte[1024];
